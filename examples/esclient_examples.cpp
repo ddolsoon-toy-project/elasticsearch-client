e4a7af09ba0ee3392a0f010f10a0863df70bc04d
ddolsoon <minseong6284@naver.com>
2020-12-24 13:51:28 +0000

    feat: 예제 추가

--- examples/esclient_examples.cpp
+++ examples/esclient_examples.cpp
@@ -1,6 +1,4 @@
-
 #include <EsClient.h>
-
 #include <iostream>
 
 int main (void)
@@ -8,6 +6,25 @@ int main (void)
 	EsClient esClient;
 
 	esClient.test();
+
+	QueryMapper queryMapper;
+
+	queryMapper.initialize("test.xml");
+
+	std::map<std::string, boost::variant<int, double, std::string>> inputParamMap;
+	inputParamMap["distance"] = "5km";
+	inputParamMap["latitude"] = 37.5358;
+	inputParamMap["longitude"] = 126.9559;
+
+	std::string resultQuery;
+	queryMapper.getQuery(resultQuery, "SearchNearestPharmacyFromDistance", inputParamMap);
+	printf("SearchNearestPharmacyFromDistance : \n%s\n", resultQuery.c_str());
+
+
+	std::map<std::string, boost::variant<int, double, std::string>> inputParamMap2;
+	inputParamMap2["name"] = "한겨례";
+	queryMapper.getQuery(resultQuery, "SearchPharmacyByName", inputParamMap2);
+	printf("SearchPharmacyByName: \n%s\n", resultQuery.c_str());
 
 	return 0;
 }
--- examples/include/EsClient.h
+++ examples/include/EsClient.h
@@ -6,8 +6,15 @@
 #ifndef __ES_CLIENT_H__ 
 #define __ES_CLIENT_H__ 
 
+#include <iostream>
 #include <cstring>
 #include <string>
+#include <spdlog/spdlog.h>
+
+#include "QueryMapper.h"
+#include "EsSearch.h"
+
+extern std::shared_ptr<spdlog::logger> g_Logger;
 
 class EsClient 
 {
@@ -16,8 +23,16 @@ class EsClient
         ~EsClient();
 		void test();
 
+	public:
+
+
 	private:
-		std::string _host;
+		std::string _strHost;
+		int _nPort;
+		int _nTimeout;
+
+		QueryMapper* _pQueryMapper;
+		EsSearch* _pEsSearch;
 };
 
 #endif 
