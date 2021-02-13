70cdcc1bffd57a48f3dc7ab0e476b980afc380c1
ddolsoon <minseong6284@naver.com>
2021-02-13 16:43:54 +0000

    feat: 예제 추가

--- examples/esclient_examples.cpp
+++ examples/esclient_examples.cpp
@@ -8,6 +8,7 @@ int main (void)
 {
 	EsClient esClient("127.0.0.1", 6200, 3000, "test.xml");
 
+	/*
 	std::map<std::string, boost::variant<int, double, std::string>> inputParamMap;
 	inputParamMap["distance"] = "5km";
 	inputParamMap["latitude"] = 37.5358;
@@ -22,6 +23,18 @@ int main (void)
 	inputParamMap2["name"] = "한겨례";
 	Response response2 = esClient.searchQuery(searchResult, "pet_pharmacy", "SearchPharmacyByName", inputParamMap2);
 	//printf("SearchPharmacyByName: \n%s\n", resultQuery.c_str());
+	
+	std::map<std::string, boost::variant<int, double, std::string>> inputParamMap2_1;
+	inputParamMap2_1["distance"] = "100000m";
+	inputParamMap2_1["latitude"] = 37.5640455;
+	inputParamMap2_1["longitude"] = 127.1663142;
+	inputParamMap2_1["type"] = "dong";
+	inputParamMap2_1["limit"] = 10;
+	Response response2_1 = esClient.searchQuery(searchResult, "pet_pharmacy", "GetPharmacyCountGroupByAddress", inputParamMap2_1);
+	printf("result.aggs: \n%s\n", searchResult.aggs.c_str());
+
+
+	return 0;
 
 	Response response3 = esClient.deleteIndex("ddolsoon_test");
 	printf("response3 : %d \n", response3.statusCode);
@@ -76,6 +89,16 @@ int main (void)
 	}
 	Response response14 = bulkProcessor.run();
 	printf("response14 : %d \n", response14.statusCode);
+
+
+	*/
+	std::map<std::string, boost::variant<int, double, std::string>> inputParamMap15;
+	inputParamMap15["channel"] = "pet-pharmacy-platform";
+	inputParamMap15["deviceToken"] = "test";
+	inputParamMap15["pushYn"] = "Y";
+	Response response15 = esClient.createDocument("push_device_info", "CreatePushDeviceInfo", inputParamMap15, "test01");
+	printf("response15 : %d \n", response15.statusCode);
+	printf("response15 : %s \n", response15.errorMessage.c_str());
 
 	return 0;
 }
